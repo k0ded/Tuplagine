2b45ac70494bd1069406c8dac20f22bcf1ff3bce
liam.sjoholm <liam.sjoholm@k0ded.com>
2024-04-04 18:13:05 +0200

    Beginning of ecs

---
+++ Tupla-Engine/Source/Tupla/Scene/ComponentManager.cpp
@@ -0,0 +1,2 @@
+#include "tgpch.h"
+#include "ComponentManager.h"
---
+++ Tupla-Engine/Source/Tupla/Scene/ComponentManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <queue>
+#include "Hot/HotDefines.h"
+
+namespace Tupla
+{
+	class ComponentManager
+	{
+	public:
+
+	private:
+		std::queue<HOTEntity> availableHotEntities;
+	};
+}
+
